b2ffa782a44a17ea3452ad2cf0c7868334d9930f
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2021-11-04 21:31:00 +0300

    feat: add solution for 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesFolder = path.join(__dirname, 'styles');
+const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+const bundleWrite = fs.createWriteStream(bundle);
+
+fs.readdir(stylesFolder, (err, dirFilesList) => { 
+  if(err) {
+    console.log(err);
+  } 
+  else {
+    dirFilesList.forEach(file => {
+      if(path.extname(file) !== '.txt') {
+        fs.createReadStream(`${stylesFolder}/${file}`, 'utf-8').pipe(bundleWrite);
+      }
+    });
+  }
+});
