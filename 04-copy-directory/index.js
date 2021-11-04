3cf4ef9cafb8ba5d8bcb66096c442bfc4969f6c0
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2021-11-04 20:43:28 +0300

    feat: add solution for 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(folderCopy, {recursive: true}, err => { // create new folder
+  if(err) {
+    console.log(err);
+  }
+});
+
+fs.readdir(folderCopy, (err, dirFilesList) => { // delete all files from new folder
+  if(err) {
+    console.log(err);
+  } 
+  else {
+    dirFilesList.forEach(file => {
+      fs.unlink(`${folderCopy}/${file}`, err => {
+        if(err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+});
+
+fs.readdir(folder, (err, dirFilesList) => { // copy all files to the new folder
+  if(err) {
+    console.log(err);
+  } 
+  else {
+    dirFilesList.forEach(file => {
+  
+      fs.copyFile(`${folder}/${file}`, `${folderCopy}/${file}`, err => {
+        if(err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+});
 
